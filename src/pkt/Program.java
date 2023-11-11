f8069f13c7ad310a19cacf76e6b786bd1b94b827
Yusuf Ozaslan <ozaslanysf@gmail.com>
2023-11-11 20:30:03 +0300

    model updated

--- src/pkt/Program.java
+++ src/pkt/Program.java
@@ -9,25 +9,34 @@ public class Program {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-        System.out.print("Wind Speed (0-25):");
-        double windSpeed = 15; //in.nextDouble();
-        System.out.print("Height (5-150):");
-        double height = 55; //in.nextDouble();
-        System.out.print("Wing Length (1-80):");
-        double wingLength = 33; //in.nextDouble();
+        System.out.print("Wind Speed in m/s(0-25): ");
+        double windSpeed = in.nextDouble();
+        
+        System.out.print("\rWing Length in meters (1-80):");
+        double wingLength = in.nextDouble();
+
+        double max = wingLength*(10/2);
+        double min = wingLength*(10/4);
+        
+        System.out.print("\rHeight in meters (" + Math.round(max) + "-" + Math.round(min) + "): ");
+        double height = in.nextDouble();
+        
+        while (height < min || height > max) {
+        	System.out.print("\rHeight in meters (" + Math.round(max) + "-" + Math.round(min) + "): ");
+        	height = in.nextDouble();
+		}
         
         try {
-        	System.out.println("tets 1");
         	WindTurbine windTurbine = new WindTurbine(windSpeed, height, wingLength);
 	        JFuzzyChart.get().chart(windTurbine.getModel());
-	      //  for(Rule r:windTurbine.getModel().getFunctionBlock("energyProduced").getFuzzyRuleBlock("ruleBlock1").getRules()) {
-	     //   	if (r.getDegreeOfSupport()>0) {
-		//			System.out.println(r);// calistirilan kurallar yazilir. Bastaki degerler kuralin etki oranini verir
-			//	} 
-	       // } 
+	        for(Rule r:windTurbine.getModel().getFunctionBlock("WindTurbine").getFuzzyRuleBlock("ruleBlock1").getRules()) {
+	        	if (r.getDegreeOfSupport()>0) {
+					System.out.println(r);// calistirilan kurallar yazilir. Bastaki degerler kuralin etki oranini verir
+				} 
+	        } 
 
 	        System.out.println(windTurbine);
-	        //JFuzzyChart.get().chart(windTurbine.getModel().getVariable("energyProduced").getDefuzzifier(), "Energy Produced", true);
+	        JFuzzyChart.get().chart(windTurbine.getModel().getVariable("energyProduced").getDefuzzifier(), "Energy Produced", true);
         }
         catch (URISyntaxException ex) {
         	System.out.println(ex.getMessage());
